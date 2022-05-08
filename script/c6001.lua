9068475d20590caf6bed5235cbfc8a9652bd2215
Kc <kc@fog.moe>
2022-05-09 02:09:57 +0800

    1

--- script/c6001.lua
+++ script/c6001.lua
@@ -2,39 +2,30 @@ local m=6001
 local cm=_G["c"..m]
 cm.name="特雷维喷泉"
 function cm.initial_effect(c)
+	--Activate
 	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DRAW)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetOperation(cm.op)
+	e1:SetTarget(cm.target)
 	e1:SetCost(cm.cost)
+	e1:SetOperation(cm.activate)
 	c:RegisterEffect(e1)
 end
-function cm.op(e,tp,eg,ep,ev,re,r,rp)
-	local e4=Effect.CreateEffect(e:GetHandler())
-	e4:SetCategory(CATEGORY_DRAW)
-	e4:SetCode(EVENT_PHASE+PHASE_STANDBY)
-	e4:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e4:SetCondition(cm.setcon)
-	e4:SetTarget(cm.tg)
-	e4:SetOperation(cm.activate)
-	e4:SetLabelObject(e3)
-	c:RegisterEffect(e4)
-end
-function cm.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,e:GetHandler()) end
-	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
-end
-function cm.setcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetLabelObject():GetLabel()==Duel.GetTurnCount() and e:GetHandler():GetFlagEffect(m)>0
-end
-function cm.tg(e,tp,eg,ep,ev,re,r,rp,chk)
+function cm.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsPlayerCanDraw(tp,2) end
 	Duel.SetTargetPlayer(tp)
 	Duel.SetTargetParam(2)
 	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
 end
-
 function cm.activate(e,tp,eg,ep,ev,re,r,rp)
 	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
 	Duel.Draw(p,d,REASON_EFFECT)
+end
+
+function cm.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,e:GetHandler()) and Duel.CheckLPCost(tp,3000) end
+	Duel.PayLPCost(tp,3000)
+	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
 end
