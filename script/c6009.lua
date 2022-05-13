e71bba63b33da9cc0b823f1b49c382daa5ad23ee
Kc <kc@fog.moe>
2022-05-13 15:25:54 +0800

    1.9

---
+++ script/c6009.lua
@@ -0,0 +1,50 @@
+local m=6009
+local cm=_G["c"..m]
+cm.name="对峙灵摆"
+function cm.initial_effect(c)
+		--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_SPSUMMON)
+	e1:SetTarget(cm.target)
+	e1:SetOperation(cm.activate)
+	e1:SetCondition(cm.condition)
+	c:RegisterEffect(e1)
+end
+function cm.cfilter(c)
+	return c:IsSummonType(SUMMON_TYPE_PENDULUM)
+end
+function cm.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetCurrentChain()==0 and eg:IsExists(cm.cfilter,1,nil) and not Duel.GetFieldCard(tp,LOCATION_PZONE,0) and not Duel.GetFieldCard(tp,LOCATION_PZONE,1)
+end
+function cm.filter(c,e,tp)
+	return c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function cm.adfilter(c)
+	return c:IsFaceup()
+end
+function cm.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(cm.filter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,2,tp,LOCATION_HAND)
+end
+function cm.activate(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,cm.filter,tp,LOCATION_HAND,0,1,2,nil,e,tp)
+	if Duel.SpecialSummon(g,SUMMON_TYPE_SPECIAL,tp,tp,false,false,POS_FACEUP) then
+		Duel.BreakEffect()
+		local sg=Duel.GetMatchingGroup(cm.adfilter,tp,LOCATION_MZONE,0,nil)
+		local tc=sg:GetFirst()
+		while tc do
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_UPDATE_ATTACK)
+			e1:SetValue(700)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+			tc:RegisterEffect(e1)
+			tc=sg:GetNext()
+		end
+	end
+end
